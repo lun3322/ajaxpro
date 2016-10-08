62017e01afc71ea68b1fb7750edbd38d28eb2f9c
欧阳悲鸿 <lun3322@126.com>
2016-10-08 11:41:25 +0800

    新增两个事件

---
+++ Configuration/HandlerConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AjaxPro.Configuration
+{
+    public delegate T Func<T>();
+
+    public class HandlerConfiguration
+    {
+        private static Func<AjaxProDefaultSettings> _defaultSettings;
+
+        public static Func<AjaxProDefaultSettings> DefaultSettings
+        {
+            get { return _defaultSettings; }
+            set { _defaultSettings = value; }
+        }
+
+        static HandlerConfiguration()
+        {
+            DefaultSettings = SetDefault;
+        }
+
+        static AjaxProDefaultSettings SetDefault()
+        {
+            return new AjaxProDefaultSettings();
+        }
+    }
+
+    public class AjaxProDefaultSettings
+    {
+        public delegate void CusEventHandler(object sender, HandlerEventArgs e);
+
+        public event CusEventHandler PreHandlerExecute;
+        public event CusEventHandler PostHandlerExecute;
+
+        internal void OnPreHandlerExecute(HandlerEventArgs e)
+        {
+            if (PreHandlerExecute != null)
+                PreHandlerExecute(this, e);
+        }
+
+        internal void OnPostHandlerExecute(HandlerEventArgs e)
+        {
+            if (PostHandlerExecute != null)
+                PostHandlerExecute(this, e);
+        }
+    }
+
+    public class HandlerEventArgs : EventArgs
+    {
+        private string _methodName;
+        private string _args;
+
+        public string MethodName
+        {
+            get { return _methodName; }
+            set { _methodName = value; }
+        }
+
+        public string Args
+        {
+            get { return _args; }
+            set { _args = value; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ajaxpro
 基于ajaxpro 9.2.17.1 版本
+
+在 Application_Start 中增加
+HandlerConfiguration.DefaultSettings = () =>
+{
+	var setting = new AjaxProDefaultSettings();
+	setting.PreHandlerExecute += (self, arg) =>
+	{
+		TKF.Common.showinfo.ShowInfo("\r\n\t{0} 方法 开始:\r\n\t参数: {1}", arg.MethodName, arg.Args);
+	};
+
+	setting.PostHandlerExecute += (self, arg) =>
+	{
+		TKF.Common.showinfo.ShowInfo("\r\n\t{0} 方法 结束:\r\n\t返回值: {1}", arg.MethodName, arg.Args);
+	};
+	return setting;
+};
